ddff901d2129549bbbd10bc59fcce13e7be34ad5
Dong557799 <3031267623@qq.com>
2026-03-20 13:43:43 +0800

    通用插件的使用

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
 import {lazyPlugin} from './directives'
-
+import {componentPlugin} from './components'
 //测试接口函数
 import { getCategory } from '@/apis/testAPI'
 getCategory().then(res => {
@@ -20,6 +20,7 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
 // //定义全局el指令绑定元素，binding.value指令等于号后面绑定的表达式的值 图片url
 // app.directive('img-lazy',{
