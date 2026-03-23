88a7edd5ef7612fa9aa467b5a5425f4c8cb75b43
Dong557799 <3031267623@qq.com>
2026-03-23 17:19:57 +0800

    头部购物车统计计算

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,6 @@
 import {defineStore} from 'pinia'
-import {ref} from 'vue'
+import {ref,computed} from 'vue'
+
 export const useCartStore=defineStore('cart',()=>{
     const cartList=ref([])
     const addCart=(goods)=>{
@@ -16,10 +17,16 @@ export const useCartStore=defineStore('cart',()=>{
         const idx=cartList.value.findIndex((item)=>skuId===item.skuId)
         cartList.value.splice(idx,1)
     }
+
+    //计算属性
+    const allCount=computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
+    const allPrice=computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
     return{
         cartList,
         addCart,
-        delCart
+        delCart,
+        allCount,
+        allPrice
     }
 },{
     persist:true,
