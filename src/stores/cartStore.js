663a29899053edc00aee083f39cfbcacdb442e02
Dong557799 <3031267623@qq.com>
2026-03-27 15:57:03 +0800

    列表购物车全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -23,16 +23,25 @@ export const useCartStore=defineStore('cart',()=>{
         const item=cartList.value.find((item)=>item.skuId===skuId)
         item.selected=selected
     }
+    //全选
+    const allCheck=(selected)=>{
+        cartList.value.forEach(item=>item.selected=selected)
+    }
+
     //计算属性
     const allCount=computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
     const allPrice=computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
+    //是否全选
+    const isAll=computed(()=>cartList.value.every((item)=>item.selected))
     return{
         cartList,
         addCart,
         delCart,
+        isAll,
         allCount,
         allPrice,
-        singleCheck
+        singleCheck,
+        allCheck
     }
 },{
     persist:true,
